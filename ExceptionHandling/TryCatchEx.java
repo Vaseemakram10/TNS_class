dfa56dd1b5af133013d3247a3f569f2512df4671
vaseem akram <154436575+Vaseemakram10@users.noreply.github.com>
2025-10-30 15:24:21 +0530

    Delete ExceptionHandling directory

--- ExceptionHandling/TryCatchEx.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.ExceptionHandling;
-
-public class TryCatchEx {
-	static void performDivision(int a,int b) {
-		System.out.println("In method");
-		int ans=a/b;
-		System.out.println("In method1");
-	}
-}
